01421186898835792c271619def4971446a1290a
Abdeljalil HOUCHOU <abdeljalil.houchou@hotmail.com>
2026-02-15 20:36:50 +0100

    pour deploiment

--- fleetrental-frontend/next.config.mjs
+++ fleetrental-frontend/next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   images: {
     remotePatterns: [
       { hostname: 'localhost', port: '8000', protocol: 'http' },
+      { hostname: '**.railway.app', protocol: 'https' },
     ],
   },
 };
